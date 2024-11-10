c942fbab848fff511e9fb258176f8176f0a9583a
ebcfireball <ebcftb@gmail.com>
2024-11-10 12:39:37 -0700

    added bid ask

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,14 @@ pub struct A {
     pub executed_price: Option<u32>,
     pub new_orff: Option<u64>,
     pub cancelled_shares: Option<u32>,
+    pub bid: Option<u32>,
+    pub ask: Option<u32>
+}
+
+pub struct Order {
+    pub price: u32,
+    pub re: u64,
+    pub shares: u32,
 }
 
 impl<R:Read> MsgStream<R> {
@@ -78,8 +86,8 @@ impl<R:Read> MsgStream<R> {
                 let orn = BigEndian::read_u64(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 let cancelled_shares = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(88),timestamp:Some(timestamp),orrf:Some(orn),cancelled_shares:Some(cancelled_shares),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None}))
-                    .or_insert(vec![A{typ:Some(88),timestamp:Some(timestamp),orrf:Some(orn),cancelled_shares:Some(cancelled_shares),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(88),timestamp:Some(timestamp),orrf:Some(orn),cancelled_shares:Some(cancelled_shares),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(88),timestamp:Some(timestamp),orrf:Some(orn),cancelled_shares:Some(cancelled_shares),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None}]);
             } else if curr == Some(65u8) {
                 //A
                 let mut data = self.buffer.drain(..37);
@@ -92,8 +100,8 @@ impl<R:Read> MsgStream<R> {
                 data.nth(7);
                 let price = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(65),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
-                    .or_insert(vec![A{typ:Some(65),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(65),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(65),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
             } else if curr == Some(80u8) {
                 //P
                 let mut data = self.buffer.drain(..45);
@@ -106,8 +114,8 @@ impl<R:Read> MsgStream<R> {
                 data.nth(7);
                 let price = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(80),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
-                    .or_insert(vec![A{typ:Some(80),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(80),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(80),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
             } else if curr == Some(85u8) {
                 //U
                 let mut data = self.buffer.drain(..36);
@@ -119,8 +127,8 @@ impl<R:Read> MsgStream<R> {
                 let shares = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 let price = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(85),timestamp:Some(timestamp),orrf:Some(og_orn),buy_sell:None,shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:Some(new_orn),cancelled_shares:None}))
-                    .or_insert(vec![A{typ:Some(85),timestamp:Some(timestamp),orrf:Some(og_orn),buy_sell:None,shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:Some(new_orn),cancelled_shares:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(85),timestamp:Some(timestamp),orrf:Some(og_orn),buy_sell:None,shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:Some(new_orn),cancelled_shares:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(85),timestamp:Some(timestamp),orrf:Some(og_orn),buy_sell:None,shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:Some(new_orn),cancelled_shares:None}]);
             } else if curr == Some(69u8) {
                 //E
                 let mut data = self.buffer.drain(..32);
@@ -130,8 +138,8 @@ impl<R:Read> MsgStream<R> {
                 let orn = BigEndian::read_u64(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 let executed_shares = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(69),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}))
-                    .or_insert(vec![A{typ:Some(69),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(69),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(69),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}]);
             } else if curr == Some(67u8) {
                 //C
                 let mut data = self.buffer.drain(..37);
@@ -143,8 +151,8 @@ impl<R:Read> MsgStream<R> {
                 data.nth(8);
                 let executed_price = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(67),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:Some(executed_price),executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}))
-                    .or_insert(vec![A{typ:Some(67),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:Some(executed_price),executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(67),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:Some(executed_price),executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(67),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:Some(executed_price),executed_shares:Some(executed_shares),new_orff:None,cancelled_shares:None}]);
             } else if curr == Some(68u8) {
                 //D
                 let mut data = self.buffer.drain(..20);
@@ -153,8 +161,8 @@ impl<R:Read> MsgStream<R> {
                 let timestamp = BigEndian::read_u48(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 let orn = BigEndian::read_u64(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(68),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
-                    .or_insert(vec![A{typ:Some(68),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(68),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(68),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:None,price:None,executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
             } else if curr == Some(70u8) {
                 //F
                 let mut data = self.buffer.drain(..41);
@@ -167,8 +175,8 @@ impl<R:Read> MsgStream<R> {
                 data.nth(7);
                 let price = BigEndian::read_u32(&[data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),data.next().unwrap(),]);
                 self.companies.entry(locate)
-                    .and_modify(|company| company.push(A{typ:Some(70),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
-                    .or_insert(vec![A{typ:Some(70),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
+                    .and_modify(|company| company.push(A{bid:None,ask:None,typ:Some(70),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:Some(buy_sell),shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}))
+                    .or_insert(vec![A{bid:None,ask:None,typ:Some(70),timestamp:Some(timestamp),orrf:Some(orn),buy_sell:None,shares:Some(shares),price:Some(price),executed_price:None,executed_shares:None,new_orff:None,cancelled_shares:None}]);
             } else if curr == Some(83u8) {
                 //S
                 self.buffer.drain(..13);
@@ -221,9 +229,121 @@ impl<R:Read> MsgStream<R> {
         Ok(self.reader.read(&mut temp_buffer)?)
     }
 
+    pub fn process_order_book(&mut self) -> Result<()> {
+        for (_loc, feed) in &mut self.companies {
+            let mut bids: Vec<Order> = vec![];
+            let mut asks: Vec<Order> = vec![];
+            for order in feed{
+                if order.typ == Some(68){
+                    //D order delete
+                    if let Some(index) = bids.iter().position(|re| Some(re.re)==order.orrf){
+                        bids.remove(index);
+                    }else {
+                        let position = asks.iter().position(|re| Some(re.re)==order.orrf);
+                        asks.remove(position.unwrap());
+                    }
+                } else if order.typ == Some(65){
+                    //A add order
+                    if order.buy_sell==Some(66){
+                        bids.push(Order{price:order.price.unwrap(),re:order.orrf.unwrap(),shares:order.shares.unwrap()});
+                    } else {
+                        asks.push(Order{price:order.price.unwrap(),re:order.orrf.unwrap(),shares:order.shares.unwrap()});
+                    }
+                } else if order.typ == Some(70) {
+                    //F add order
+                    if order.buy_sell==Some(66){
+                        bids.push(Order{price:order.price.unwrap(),re:order.orrf.unwrap(),shares:order.shares.unwrap()});
+                    } else {
+                        asks.push(Order{price:order.price.unwrap(),re:order.orrf.unwrap(),shares:order.shares.unwrap()});
+                    }
+                } else if order.typ == Some(88) {
+                    //X order cancel
+                    if let Some(index) = bids.iter().position(|re| Some(re.re)==order.orrf){
+                        if let Some(ord) = bids.get_mut(index){
+                            ord.shares -= &order.cancelled_shares.unwrap();
+                        }
+                    }else {
+                        let position = asks.iter().position(|re| Some(re.re)==order.orrf);
+                        if let Some(ord) = asks.get_mut(position.unwrap()){
+                            ord.shares -= &order.cancelled_shares.unwrap();
+                        }
+                    }
+                } else if order.typ == Some(67) {
+                    //C order executed do something
+                    if let Some(index) = bids.iter().position(|re| Some(re.re) == order.orrf){
+                        if bids[index].shares - order.executed_shares.unwrap()==0{
+                            bids.remove(index);
+                        } else{
+                            bids[index].shares = bids[index].shares-order.executed_shares.unwrap();
+                        }
+                    } 
+                    if let Some(index) = asks.iter().position(|re| Some(re.re)==order.orrf){
+                        if asks[index].shares - order.executed_shares.unwrap()==0{
+                            asks.remove(index);
+                        } else{
+                            asks[index].shares = asks[index].shares-order.executed_shares.unwrap();
+                        }
+                    }
+                } else if order.typ == Some(69) {
+                    //E order executed do something
+                    if let Some(index) = bids.iter().position(|re| Some(re.re) == order.orrf){
+                        if bids[index].shares - order.executed_shares.unwrap()==0{
+                            bids.remove(index);
+                        } else{
+                            bids[index].shares = bids[index].shares-order.executed_shares.unwrap();
+                        }
+                    } 
+                    if let Some(index) = asks.iter().position(|re| Some(re.re)==order.orrf){
+                        if asks[index].shares - order.executed_shares.unwrap()==0{
+                            asks.remove(index);
+                        } else{
+                            asks[index].shares = asks[index].shares-order.executed_shares.unwrap();
+                        }
+                    }
+                } else if order.typ == Some(85) {
+                    //U order replace
+                    if let Some(index) = bids.iter().position(|re| Some(re.re)==order.orrf){
+                        if let Some(ord) = bids.get_mut(index){
+                            ord.price = order.price.unwrap();
+                            ord.shares = order.shares.unwrap();
+                            ord.re = order.new_orff.unwrap();
+                        }
+                    }else {
+                        let position = asks.iter().position(|re| Some(re.re)==order.orrf);
+                        if let Some(ord) = asks.get_mut(position.unwrap()){
+                            ord.price = order.price.unwrap();
+                            ord.shares = order.shares.unwrap();
+                            ord.re = order.new_orff.unwrap();
+                        }
+                    }
+                } else if order.typ == Some(80) {
+                    //P trade message do something
+                }
+                bids.sort_by_key(|ord| ord.price);
+                bids.reverse();
+                asks.sort_by_key(|ord| ord.price);
+                //go back to how it was with the 
+                if bids.len()>0 && asks.len()>0{
+                    order.ask = Some(asks[0].price);
+                    order.bid = Some(bids[0].price);
+                }else if bids.len()>0 && asks.len()==0{
+                    order.ask = Some(0);
+                    order.bid = Some(bids[0].price);
+                }else if bids.len()==0 && asks.len()>0{
+                    order.ask = Some(asks[0].price);
+                    order.bid = Some(0);
+                } else{
+                    order.bid = Some(0);
+                    order.ask = Some(0);
+                }
+            }
+        }
+        Ok(())
+    }
+
     pub fn write_companies(&mut self) -> Result<()> {
         for (loc, feed) in &self.companies{
-            let mut wtr = Writer::from_path(format!("{}oct302019.csv",loc))?;
+            let mut wtr = Writer::from_path(format!("data/{}oct302019.csv",loc))?;
             for msg in feed {
                 wtr.serialize(msg)?;
             }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() -> io::Result<()>{
     let mut test_read = MsgStream::from_gz("oct302019ITCH.gz").unwrap();
     let start = Instant::now();
     let _a = test_read.process_bytes();
+    let _b = test_read.process_order_book();
+    let _c = test_read.write_companies();
     let finish = Instant::elapsed(&start);
     println!("{:?}",finish);
     Ok(())
