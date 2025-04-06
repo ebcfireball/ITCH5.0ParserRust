0df72975ced935c6d9bb7e52d698256b3d974e20
Edward Cannon <59624867+ebcfireball@users.noreply.github.com>
2025-04-05 22:11:58 -0600

    Delete itch-master directory

--- itch-master/ITCH/analysis/cython/shrs_from_dict_setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(name="shrs_from_dict", ext_modules=cythonize('shrs_from_dict.pyx'))
--- itch-master/ITCH/processing/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import partial_read_buffer
-from . import grouped_order
-from . import decode
-from .book_status import BookStatus
-
